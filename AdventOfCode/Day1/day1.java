8ed5aab3676fe3bd730833c8b24b8d0bcbd31ecd
Nolan Pelino <npelino@email.sc.edu>
2022-12-10 20:27:48 -0500

    Commenting

--- AdventOfCode/Day1/day1.java
+++ AdventOfCode/Day1/day1.java
@@ -8,38 +8,38 @@ public class day1 {
     String file;
 
     public day1(String input){
-        this.file = input;
+        this.file = input;  // Object takes in the requested input file
     }
 
     public static void main(String[] args) {
         day1 answer = new day1("Personal-Projects/AdventOfCode/Day1/input.txt");
-        answer.readCalorieCounts();
+        answer.readCalorieCounts();  // Read the input file and calculate the answer. Prints answer to console
     }
 
     public void readCalorieCounts() {
         ArrayList<Integer> calorieCounts = new ArrayList<Integer>();
-        Integer group = 0;
+        Integer group = 0;  // Current gorup's calorie count
         Integer firstGroup = 0;
         Integer secondGroup = 0;
         Integer thirdGroup = 0;
         try (Scanner scan = new Scanner(new File(this.file))) {
-            while (scan.hasNextLine()) {
+            while (scan.hasNextLine()) {  // While the file is not finished
                 String line = scan.nextLine().trim();
-                if (!line.isEmpty()) {
+                if (!line.isEmpty()) {  // If the line is not empty
                     group = group + Integer.parseInt(line);
-                } else {
+                } else {  // If the line is empty, add the current group's calorie count to the list
                     calorieCounts.add(group);
-                    if (group > firstGroup) {
+                    if (group > firstGroup) {  // If the largest
                         thirdGroup = secondGroup;
                         secondGroup = firstGroup;
                         firstGroup = group;
-                    } else if (group >= secondGroup && group < firstGroup) {
+                    } else if (group >= secondGroup && group < firstGroup) {  // If the second largest
                         thirdGroup = secondGroup;
                         secondGroup = group;
-                    } else if (group >= thirdGroup && group < secondGroup) {
+                    } else if (group >= thirdGroup && group < secondGroup) {  // If the third largest
                         thirdGroup = group;
                     }
-                    group = 0;
+                    group = 0;  // Reset group count
                 }
             }
             System.out.println(firstGroup + secondGroup + thirdGroup);
